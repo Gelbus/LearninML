9e4404fe2653d71c0ff969d2c2a2e414d4769910
Gleb <GNechto@yandex.ru>
2025-07-06 19:14:41 +0300

    небольшие правки 2.2

--- Stage2/main.py
+++ Stage2/main.py
@@ -2,8 +2,11 @@ import Perceptron
 import load_data as data
 import visualisation as vis
 
-ppn: Perceptron = Perceptron.Perceptron(eta=0.1, n_iter=10)
-ppn.fit(data.X, data.y)
+def stage_2_2():
+    ppn: Perceptron = Perceptron.Perceptron(eta=0.1, n_iter=10)
+    ppn.fit(data.X, data.y)
 
-vis.plot_errors(ppn.errors_)
-vis.plot_decision_regions(data.X, data.y, classifier=ppn)
+    vis.plot_errors(ppn.errors_)
+    vis.plot_decision_regions(data.X, data.y, classifier=ppn)
+
+stage_2_2()
