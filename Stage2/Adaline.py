e39bdfb5018fc1f59e8db850ea9f522c46ac2599
Gleb <GNechto@yandex.ru>
2025-07-19 21:33:05 +0300

    Завершил главу 2

--- Stage2/Adaline.py
+++ Stage2/Adaline.py
@@ -42,3 +42,75 @@ class AdalineGD:
 
     def predict(self, X):
         return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+
+
+class AdalineSGD:
+    """
+    eta: float
+        Скорость обучения 0 - 1
+    n_iter: int
+        Количество проходов по обучающему набору.
+    random_state: int
+        Опорное значение генератора случайных чисел для инициализации весов.
+    """
+
+    def __init__(self, eta: float, n_iter: int, shuffle: bool = True, random_state: int = None) -> None:
+        self.eta: float = eta
+        self.n_iter: int = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        self.random_state: float = random_state
+
+
+    def fit(self, X, y):
+        self._initialized_weights(X.shape[1])
+        self.losses_: List[np.floating] = []
+
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            losses = []
+            for xi, target in zip(X, y):
+                losses.append(self._update_weights(xi, target))
+            avg_loss = np.mean(losses)
+            self.losses_.append(avg_loss)
+        return self
+
+    def partial_fit(self, X, y):
+        if not self.w_initialized:
+            self._initialized_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def _shuffle(self, X, y):
+        r = self.rgen.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialized_weights(self, m):
+        self.rgen = np.random.RandomState(self.random_state)
+        self.w_ = self.rgen.normal(loc=0.0, scale=0.01, size=m)
+        self.b_ = np.float32(0.0)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        output = self.activation(self.net_input(xi))
+        error = target - output
+        self.w_ += self.eta * 2.0 * xi * error
+        self.b_ += self.eta * error
+        loss = error**2
+
+        return loss
+
+    def net_input(self, X):
+        return np.dot(X, self.w_) + self.b_
+
+    def activation(self, X):
+        return X
+
+    def predict(self, X):
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+
--- Stage2/main.py
+++ Stage2/main.py
@@ -12,17 +12,28 @@ def stage_2_2():
     # vis.plot_errors(ppn.errors_)
     # vis.plot_decision_regions(data.X, data.y, classifier=ppn)
 
-    adl1: Adaline = Adaline.AdalineGD(eta=0.01, n_iter=15)
-    adl1.fit(data.X, data.y)
-    adl2: Adaline = Adaline.AdalineGD(eta=0.01, n_iter=15)
-    adl2.fit(data.X, data.y)
-    plot_losses(adl1, adl2)
+    # adl1: Adaline = Adaline.AdalineGD(eta=0.01, n_iter=15)
+    # adl1.fit(data.X, data.y)
+    # adl2: Adaline = Adaline.AdalineGD(eta=0.01, n_iter=15)
+    # adl2.fit(data.X, data.y)
+    # plot_losses(adl1, adl2)
 
 
-    ada_gd = Adaline.AdalineGD(eta=0.5, n_iter=15)
-    ada_gd.fit(data.X_std, data.y)
-    plot_decision_regions(data.X_std, data.y, classifier=ada_gd)
-    plot_losses(ada_gd, ada_gd)
+    # ada_gd = Adaline.AdalineGD(eta=0.5, n_iter=15, random_state=1)
+    # ada_gd.fit(data.X_std, data.y)
+    # plot_decision_regions(data.X_std, data.y, classifier=ada_gd)
+    # plot_losses(ada_gd)
+
+    ada_sgd = Adaline.AdalineSGD(eta=0.01, n_iter=2, random_state=1)
+    ada_sgd.fit(data.X_std, data.y)
+    plot_decision_regions(data.X_std, data.y, classifier=ada_sgd)
+    plot_losses(ada_sgd)
+    print(ada_sgd.w_)
+
+    ada_sgd.partial_fit(data.X_std[0, :], data.y[0])
+    plot_decision_regions(data.X_std, data.y, classifier=ada_sgd)
+    plot_losses(ada_sgd)
+    print(ada_sgd.w_)
 
 
 stage_2_2()
